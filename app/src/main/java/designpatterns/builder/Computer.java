3c151503f5e682658fdcace18a05e34574550b73
Mihail Marian <mihailmarian12@gmail.com>
2022-09-18 10:10:00 +0100

    builder method

---
+++ app/src/main/java/designpatterns/builder/Computer.java
@@ -0,0 +1,65 @@
+package designpatterns.builder;
+
+public class Computer {
+
+    // required parameters
+    private int HddInGb;
+    private int RamInGb;
+
+    // optional parameters
+    private boolean HasGraphicsCard;
+    private boolean HasBluetooth;
+
+    public int getHddInGb() {
+        return HddInGb;
+    }
+
+    public int getRamInGb() {
+        return RamInGb;
+    }
+
+    public boolean hasGraphicsCard() {
+        return HasGraphicsCard;
+    }
+
+    public boolean hasBluetooth() {
+        return HasBluetooth;
+    }
+
+    private Computer(ComputerBuilder builder) {
+        this.HddInGb = builder.HddInGb;
+        this.RamInGb = builder.RamInGb;
+        this.HasGraphicsCard = builder.HasGraphicsCard;
+        this.HasBluetooth = builder.HasBluetooth;
+    }
+
+    // Builder class
+    public static class ComputerBuilder {
+        // required parameters
+        private int HddInGb;
+        private int RamInGb;
+
+        // optional parameters
+        private boolean HasGraphicsCard;
+        private boolean HasBluetooth;
+
+        public ComputerBuilder(int hdd, int ram) {
+            this.HddInGb = hdd;
+            this.RamInGb = ram;
+        }
+
+        public ComputerBuilder setHasGraphicsCard(boolean hasGraphicsCard) {
+            this.HasGraphicsCard = hasGraphicsCard;
+            return this;
+        }
+
+        public ComputerBuilder setHasBluetooth(boolean hasBluetooth) {
+            this.HasBluetooth = hasBluetooth;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
---
+++ app/src/test/java/designpatterns/builder/ComputerTest.java
@@ -0,0 +1,21 @@
+package designpatterns.builder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import designpatterns.builder.Computer.ComputerBuilder;
+
+public class ComputerTest {
+    @Test
+    void testComputer() {
+        ComputerBuilder computerBuilder = new Computer.ComputerBuilder(500, 12);
+        computerBuilder.setHasBluetooth(true);
+        Computer computer = computerBuilder.build();
+
+        assertEquals(true, computer.hasBluetooth());
+        assertEquals(false, computer.hasGraphicsCard());
+        assertEquals(500, computer.getHddInGb());
+        assertEquals(12, computer.getRamInGb());
+    }
+}
