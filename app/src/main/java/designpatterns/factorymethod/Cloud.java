973ff7ec739ef56f6a87e7325f8d148cd7d54079
Mihail Marian <mihailmarian12@gmail.com>
2022-09-19 17:38:40 +0100

    factory method

---
+++ app/src/main/java/designpatterns/factorymethod/Cloud.java
@@ -0,0 +1,27 @@
+package designpatterns.factorymethod;
+
+public abstract class Cloud {
+    private String logs;
+
+    public void startOneServer() {
+        this.logs = this.createServerFactoryMethod();
+    }
+
+    public abstract String createServerFactoryMethod();
+
+    public String getLogs() {
+        return this.logs;
+    }
+}
+
+class AwsCloud extends Cloud {
+    public String createServerFactoryMethod() {
+        return "AWS server created";
+    }
+}
+
+class GcpCloud extends Cloud {
+    public String createServerFactoryMethod() {
+        return "GCP server created";
+    }
+}
---
+++ app/src/test/java/designpatterns/factorymethod/CloudTest.java
@@ -0,0 +1,18 @@
+package designpatterns.factorymethod;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CloudTest {
+    @Test
+    void testCreateServers() {
+        Cloud awsCloud = new AwsCloud();
+        awsCloud.startOneServer();
+        assertEquals("AWS server created", awsCloud.getLogs());
+
+        Cloud gcpCloud = new GcpCloud();
+        gcpCloud.startOneServer();
+        assertEquals("GCP server created", gcpCloud.getLogs());
+    }
+}
